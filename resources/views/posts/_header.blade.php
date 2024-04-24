ea2fd04cf13d34d823ccf7568feca943cdd40b9a
= <75562501+hirezadehghani@users.noreply.github.com>
2024-04-24 11:15:28 +0330

    feat: change header slider text

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -9,9 +9,9 @@
 	          	</div>
 		          <div class="one-forth d-flex  align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
 		          	<div class="text">
-		          		<span class="subheading">سلام!</span>
+		          		<span class="subheading">سلام! از دیدنتون خوشحالم</span>
 			            <h1 class="mb-4 mt-3">من <span>رضا دهقانی هستم</span></h1>
-			            <h2 class="mb-4">توسعه دهنده وب</h2>
+			            <h2 class="mb-4">عاشق خلق ایده‌های نو برای جهان بهتر</h2>
 			            <p>
 						<a href="#contact-section" class="btn btn-primary py-3 px-4">درخواست همکاری</a>
 						<a href="#projects-section" class="btn btn-white btn-outline-white py-3 px-4">نمونه کارهای من</a>
@@ -31,7 +31,7 @@
 	          	</div>
 		          <div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
 		          	<div class="text">
-					  <span class="subheading">سلام!</span>
+					  <span class="subheading"></span>
 			            <h1 class="mb-4 mt-3">من یک <span>مهندس نرم افزار</span>هستم</h1>
 			            <p><a href="#" class="btn btn-primary py-3 px-4">درخواست همکاری</a> <a href="#projects-section" class="btn btn-white btn-outline-white py-3 px-4">نمونه کارهای من</a></p>
 		            </div>
